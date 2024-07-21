5ecc1031fbf55af99018b7716c63e13d194298ca
KanaKarimata <kana.lt22@gmail.com>
2024-07-22 02:08:08 +0900

    サイネージURL吐き出し欄作成

--- src/views/TimeSchedule/Confirm.vue
+++ src/views/TimeSchedule/Confirm.vue
@@ -38,13 +38,23 @@
         </div>
       </div>
 
+      <div class="card main-contents" v-if="this.time_schedule.publish_status_id === 1">
+        <div class="card-content">
+          <div>サイネージ時刻表URL</div>
+          <div style="display: flex;">
+            <input class="input" type="text" :value="this.url">
+            <button class="button" @click="this.copyUrlValue()">Copy!</button>
+          </div>
+        </div>
+      </div>
+
       <div class="card main-contents">
         <div class="card-content">
           <table class="table detail-index-table">
             <tbody class="table">
               <tr>
                 <th>運航ルール名</th>
-                <td>{{ this.time_schedule_name }}</td>
+                <td>{{ this.time_schedule.time_schedule_name }}</td>
               </tr>
               <tr>
                 <th>終日運休ボタンチェック</th>
@@ -84,44 +94,6 @@
             </tbody>
           </table>
 
-          <!-- <div class="table" v-for="(item, index) in this.timeScheduleDetailList" :key="item.id">
-            
-            <tr>
-              <td>No.{{ index + 1 }} [時間]</td>
-              <td>{{ formatDate(item.departure_time) }}</td>
-            </tr>
-            <tr>
-              <td>No.{{ index + 1 }} [運航状況]</td>
-              <td>{{ item.operation_status_info ? item.operation_status_info.operations_status_type : '' }}</td>
-            </tr>
-            <tr>
-              <td>No.{{ index + 1 }} [詳細]</td>
-              <td>{{ item.operation_status_detail_info ? item.operation_status_detail_info.operation_status_detail : '' }}</td>
-            </tr>
-            <tr>
-              <td>No.{{ index + 1 }} [詳細コメント]</td>
-              <td>{{ item.detail_comment }}</td>
-            </tr>
-            <tr>
-              <td>No.{{ index + 1 }} [備考]</td>
-              <td>{{ item.memo }}</td>
-            </tr>
-
-            <div>
-              <div>No.{{ index + 1 }} [運航状況]</div>
-              <div>No.{{ index + 1 }} [詳細]</div>
-              <div>No.{{ index + 1 }} [詳細コメント]</div>
-              <div>No.{{ index + 1 }} [備考]</div>
-            </div>
-            <div>
-              <div>チェックを入れるとサイネージが終日運休画面に変わります</div>
-              <div>{{ item.departure_time }}</div>
-              <div>{{ setViewData(item.operation_status_id) }}</div>
-              <div>{{ setViewData(item.operation_status_detail_id) }}</div>
-              <div>{{ setViewData(item.detail_comment) }}</div>
-              <div>{{ setViewData(item.memo) }}</div>
-            </div> -->
-          <!-- </div> -->
         </div>
       </div>
     </div>
@@ -132,18 +104,21 @@
 import axiosInstance from '../../../src/axios'
 import moment from 'moment';
 import { mapGetters, mapActions } from 'vuex';
+import { toast } from 'bulma-toast'
 
 export default {
   name: 'Confirm',
   data() {
     return{
-      time_schedule_name: '',
+      time_schedule: [],
       timeScheduleDetailList: [],
+      url: ''
     }
   },
   created() {
     this.getTimeScheduleDetailList()
     this.getOperationRuleInfo(this.$route.params.operation_rule_id)
+    this.url = 'http://localhost:8080/operation-rule/signage/' + this.$route.params.operation_rule_id + '/time-schedule/' + this.$route.params.time_schedule_id
   },
   mounted() {
     document.title = '時刻表-登録内容確認 | シーバス'
@@ -161,7 +136,7 @@ export default {
           }})
         console.log('APIレスポンス:', response.data)
         this.timeScheduleDetailList = response.data.scheduleDetails
-        this.time_schedule_name = response.data.time_schedule.time_schedule_name
+        this.time_schedule = response.data.time_schedule
       } catch (error) {
         console.error('APIエラー:', error.response ? error.response.data : error.message)
       }
@@ -171,6 +146,18 @@ export default {
     },
     formatDate(time) {
       return moment(time, 'HH:mm:ss').format('HH:mm');
+    },
+    copyUrlValue() {
+      navigator.clipboard.writeText(this.url)
+
+      toast({
+            message: 'コピーしました！',
+            type: 'is-success',
+            dismissible: true,
+            pauseOnHover: true,
+            duration: 2000,
+            position: 'bottom-right',
+          });
     }
   }
 }
